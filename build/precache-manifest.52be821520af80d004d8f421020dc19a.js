68b4feedd60e1496a08f67601b73dc222e251121
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-09-16 04:23:04 +0200

    init commit

---
+++ build/precache-manifest.52be821520af80d004d8f421020dc19a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed1924e6dd015485c132df29f54d6c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aeb3b83837941986e71",
+    "url": "/static/css/main.964e94ee.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aeb3b83837941986e71",
+    "url": "/static/js/main.ea247911.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
---
+++ src/data.js
@@ -0,0 +1,84 @@
+const menu = [
+  {
+    id: 1,
+    title: 'Starting the climb',
+    category: 'Krakow',
+    price: 'August 2022',
+    img: './images/1.jpg',
+    desc: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+        sapiente nihil quasi omnis esse.`,
+  },
+  {
+    id: 2,
+    title: 'Closer to the top',
+    category: 'Gdansk',
+    price: 'August 2022',
+    img: './images/2.jpg',
+    desc: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+        sapiente nihil quasi omnis esse.`,
+  },
+  {
+    id: 3,
+    title: 'Krakow Square',
+    category: 'Warsow',
+    price: 'August 2022',
+    img: './images/3.jpg',
+    desc: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+        sapiente nihil quasi omnis esse.`,
+  },
+  {
+    id: 4,
+    title: 'Fleamarket camera',
+    category: 'Krakow',
+    price: 'August 2022',
+    img: './images/4.jpg',
+    desc: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+        sapiente nihil quasi omnis esse.`,
+  },
+  {
+    id: 5,
+    title: 'University garden',
+    category: 'Gdansk',
+    price: 'August 2022',
+    img: './images/5.jpg',
+    desc: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+        sapiente nihil quasi omnis esse.`,
+  },
+  {
+    id: 6,
+    title: 'Photo-ception',
+    category: 'Warsow',
+    price: 'August 2022',
+    img: './images/6.jpg',
+    desc: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+        sapiente nihil quasi omnis esse.`,
+  },
+  {
+    id: 7,
+    title: 'Gdansk market',
+    category: 'Krakow',
+    price: 'August 2022',
+    img: './images/7.jpg',
+    desc: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+        sapiente nihil quasi omnis esse.`,
+  },
+  {
+    id: 8,
+    title: 'Guided walking tour',
+    category: 'Gdansk',
+    price: 'August 2022',
+    img: './images/8.jpg',
+    desc: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+        sapiente nihil quasi omnis esse. `,
+  },
+  {
+    id: 9,
+    title: 'By the river',
+    category: 'Warsow',
+    price: 'August 2022',
+    img: './images/9.jpg',
+    desc: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+        sapiente nihil quasi omnis esse.`,
+  },
+]
+export default menu
